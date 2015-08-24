fc76dbd6f241d668a2c503b08d9aad02b903c8fb
Jeff Gerlach <jeffgerlach06@gmail.com>
2015-08-24 05:34:50 +0000

    Added form validation for place name. Redirect to place show page when a place is created. (Lesson 27)

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,13 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-		current_user.places.create(place_params)
-		redirect_to root_path
+		@place = current_user.places.create(place_params)
+		if @place.valid?
+			#redirect_to root_path
+			redirect_to place_path(@place)
+		else
+			render :new, :status => :unprocessable_entity
+		end
 	end
 
 	def show
@@ -33,6 +38,7 @@ class PlacesController < ApplicationController
 		end
 
 		@place.update_attributes(place_params)
+		#redirect_to root_path
 		redirect_to place_path(@place)
 	end
 
@@ -41,7 +47,7 @@ class PlacesController < ApplicationController
 		if @place.user != current_user
 			return render :text => 'Not Allowed!', :status => :forbidden
 		end
-		
+
 		@place.destroy
 		redirect_to root_path
 	end
